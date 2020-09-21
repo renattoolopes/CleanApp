87727619b679f392627cfa2b85d2b61f98d891da
Renato Lopes <renattoolopes@gmail.com>
2020-09-20 22:55:40 -0300

    Created integration test to AddAccount

---
+++ UseCasesIntegrationTests/AddAccountIntegrationTests.swift
@@ -0,0 +1,36 @@
+//
+//  AddAccountIntegrationTests.swift
+//  UseCasesIntegrationTests
+//
+//  Created by Renato Lopes on 20/09/20.
+//  Copyright © 2020 Renato Lopes. All rights reserved.
+//
+
+import XCTest
+import Data
+import Infra
+import Domain
+
+class AddAccountIntegrationTests: XCTestCase {
+
+    func test_add_account() {
+        let alamofireAdapter: AlamofireAdapter = AlamofireAdapter()
+        let url: URL = URL(string: "https://clean-node-api.herokuapp.com/api/signup")!
+        let accountModel: AddAccountModel = AddAccountModel(name: "Renato Lopes", email: "renattoolopes@gmail.com", password: "123123", passwordConfirmation: "123123")
+        let sut: RemoteAddAccount = RemoteAddAccount(url: url, httpPostClient: alamofireAdapter)
+        
+        let promise = expectation(description: "SignUp")
+        // Call add to signup
+        sut.add(account: accountModel) { (result) in
+            switch result {
+            case .success(let account):
+                XCTAssertNotNil(account.id)
+                XCTAssertEqual(accountModel.name, account.name)
+                XCTAssertEqual(accountModel.email, account.email)
+            case .failure(let error): XCTFail("Expected Success, but returned: \(error)")
+            }
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 5)
+    }
+}
