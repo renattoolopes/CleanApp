8a814d9ba055d2a4ceb273fd17af36be49491993
Renato Lopes <renattoolopes@gmail.com>
2020-09-20 21:21:25 -0300

    Moved AlamofireAdapter to Infra layer and created file to URLProtocolStub

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -8,6 +8,7 @@
 
 import XCTest
 import Data
+import Infra
 import Alamofire
 
 class AlamofireAdapterTests: XCTestCase {
@@ -96,86 +97,4 @@ extension AlamofireAdapterTests {
     }
 }
 
-class AlamofireAdapter: HttpPostClient {
-    private let session: Session
-    
-    init(session: Session = .default) {
-        self.session = session
-    }
-    
-    func post(to url: URL, with data: Data?, completion: @escaping (Result<Data?, HttpClientError>) -> Void) {
-        var json: [String: Any]?
-        defer {
-            session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).responseData { (response) in
-                guard let statusCode: Int = response.response?.statusCode else { return completion(.failure(.noConnectivity))}
-                switch response.result {
-                case .failure: completion(.failure(.noConnectivity))
-                case .success(let data):
-                    switch statusCode {
-                    case 204:
-                        completion(.success(nil))
-                    case 200...299:
-                        completion(.success(data))
-                    case 401:
-                        completion(.failure(.unauthorized))
-                    case 403:
-                        completion(.failure(.forbidden))
-                    case 400...499:
-                        completion(.failure(.badRequest))
-                    case 500...599:
-                        completion(.failure(.serverError))
-                    default:
-                        completion(.failure(.noConnectivity))
-                    }
-                }
-            }.resume()
-        }
-        
-        guard let data: Data = data else { return }
-        json = data.convertToJson()
-    }
-}
 
-class URLStubProtocol: URLProtocol {
-    // Created Observable Pattern
-    private static var emit: ((URLRequest) -> Void)? = nil
-    static var error: Error?
-    static var data: Data?
-    static var response: HTTPURLResponse?
-    
-    override class func canInit(with request: URLRequest) -> Bool {
-        return true
-    }
-    
-    static func simulate(data: Data?, response: HTTPURLResponse?, error: Error?) {
-        URLStubProtocol.error = error
-        URLStubProtocol.response = response
-        URLStubProtocol.data = data
-    }
-    
-    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
-        return request
-    }
-    
-    override func startLoading() {
-        URLStubProtocol.emit?(request)
-        if let data: Data = URLStubProtocol.data {
-            client?.urlProtocol(self, didLoad: data)
-        }
-        
-        if let response: HTTPURLResponse = URLStubProtocol.response {
-            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
-        }
-        
-        if let error: Error = URLStubProtocol.error {
-            client?.urlProtocol(self, didFailWithError: error)
-        }
-        client?.urlProtocolDidFinishLoading(self)
-    }
-    
-    override func stopLoading() {}
-    
-    static func observerRequest(completion: @escaping (URLRequest) -> Void) {
-        URLStubProtocol.emit = completion
-    }
-}
