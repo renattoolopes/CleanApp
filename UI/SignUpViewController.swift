e73409c45a9ba4108cd08d1bd71246dc6892e3bd
Renato Lopes <renattoolopes@gmail.com>
2020-09-29 22:30:34 -0300

    Created SignupViewController and Storyboard

---
+++ UI/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  UI
+//
+//  Created by Renato Lopes on 29/09/20.
+//  Copyright © 2020 Renato Lopes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class SignUpViewController: UIViewController {
+    // MARK: - Outlets
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
