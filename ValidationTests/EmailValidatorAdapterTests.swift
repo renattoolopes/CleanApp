209426c34f531a1bfdbda17f10e07cc4ca1a6e51
Renato Lopes <renattoolopes@gmail.com>
2020-09-30 20:38:33 -0300

    Created validation layer

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,14 @@
+//
+//  EmailValidatorAdapterTests.swift
+//  ValidationTests
+//
+//  Created by Renato Lopes on 30/09/20.
+//  Copyright © 2020 Renato Lopes. All rights reserved.
+//
+
+import XCTest
+@testable import Validation
+
+class EmailValidatorAdapterTests: XCTestCase {
+    func test_() {}
+}
