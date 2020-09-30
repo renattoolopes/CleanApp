cada46fca3c489e486de62455e3bc5a651135d55
Renato Lopes <renattoolopes@gmail.com>
2020-09-29 21:47:45 -0300

    Added UI Layer

---
+++ UITests/SignUpViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpViewController.swift
+//  UITests
+//
+//  Created by Renato Lopes on 29/09/20.
+//  Copyright © 2020 Renato Lopes. All rights reserved.
+//
+
+import XCTest
+@testable import UI
+
+class SignUpViewController: XCTestCase {
+
+    func test_() { }
+    
+}
