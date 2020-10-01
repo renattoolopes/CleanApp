370a52f2938b460714b51057fd4ab7486efd0669
Renato Lopes <renattoolopes@gmail.com>
2020-09-30 21:10:03 -0300

    Added Main layer

---
+++ Main/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Main
+//
+//  Created by Renato Lopes on 30/09/20.
+//  Copyright © 2020 Renato Lopes. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
