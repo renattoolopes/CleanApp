29e0ac7af14c381b643e0949f88a34584951acca
Renato Lopes <renattoolopes@gmail.com>
2021-01-13 19:58:58 -0300

    Add Authentication UseCase

---
+++ Domain/UseCases/Authentication.swift
@@ -0,0 +1,29 @@
+//
+//  Authentication.swift
+//  Domain
+//
+//  Created by Renato Lopes on 13/01/21.
+//  Copyright © 2021 Renato Lopes. All rights reserved.
+//
+
+import Foundation
+
+public protocol Authentication {
+    // MARK: - Type Values
+    typealias AuthenticationResult = (Result<AccountModel, DomainError>) -> Void
+    
+    // MARK: - Methods
+    func auth(account: AddAccountModel, compleiton: @escaping AuthenticationResult)
+}
+
+public struct AuthenticationModel: Model {
+    // MARK: - Public Properties
+    public var email: String
+    public var password: String
+    
+    // MARK: - Initializers
+    public init(email: String, password: String) {
+        self.email = email
+        self.password = password
+    }
+}
