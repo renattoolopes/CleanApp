e7f0991dbf25e7d24686c63135e7fcb9a076b930
Renato Lopes <renattoolopes@gmail.com>
2021-01-18 21:31:59 -0300

    ensure WellcomeRouter calls nav with signUpFactory on gotoSignUp

--- UITests/Scenes/Wellcome/WellcomeRouterTests.swift
+++ UITests/Scenes/Wellcome/WellcomeRouterTests.swift
@@ -14,26 +14,53 @@ import UI
 public final class WellcomeRouter {
     private let nav: NavigationController
     private let loginFactory: () -> LoginViewController
-    
-    public init(nav: NavigationController, loginFactory: @escaping () -> LoginViewController) {
+    private let signUpFactory: () -> SignUpViewController
+
+    public init(nav: NavigationController,
+                loginFactory: @escaping () -> LoginViewController,
+                signUpFactory: @escaping () -> SignUpViewController) {
         self.nav = nav
         self.loginFactory = loginFactory
+        self.signUpFactory = signUpFactory
     }
     
     public func gotoLogin() {
         let controller: LoginViewController = loginFactory()
         nav.push(viewController: controller)
     }
+    
+    public func gotoSignUp() {
+        let controller: SignUpViewController = signUpFactory()
+        nav.push(viewController: controller)
+    }
 }
 
 class WellcomeRouterTests: XCTestCase {
     func test_go_to_login_calls_correct_controller() {
-        let nav = NavigationController()
-        let sut = WellcomeRouter(nav: nav, loginFactory: { try! LoginViewController.instantiate() })
+        let (sut, nav) = makeSut()
         sut.gotoLogin()
         XCTAssertEqual(1, nav.viewControllers.count)
         XCTAssertTrue(nav.viewControllers[0] is LoginViewController)
+
+    }
+    
+    func test_go_to_signUp_calls_correct_controller() {
+        let (sut, nav) = makeSut()
+        sut.gotoSignUp()
+        XCTAssertEqual(1, nav.viewControllers.count)
+        XCTAssertTrue(nav.viewControllers[0] is SignUpViewController)
+    }
+}
+
+
+extension WellcomeRouterTests {
+    func makeSut() -> (sut: WellcomeRouter, nav: NavigationController) {
+        let nav = NavigationController()
+        let sut = WellcomeRouter(nav: nav,
+                                 loginFactory: { try! LoginViewController.instantiate() },
+                                 signUpFactory: { try! SignUpViewController.instantiate() })
         checkMemoryLeak(for: nav)
         checkMemoryLeak(for: sut)
+        return (sut, nav)
     }
 }
